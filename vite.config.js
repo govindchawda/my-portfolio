d2e9a4bad05e1255bb3aa3795aea4edd6fc475cd
govindchawda <gchawda466@gmail.com>
2025-07-28 15:28:07 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
- base: '/my-portfolio/',// 🔁 Change this to your repo name
+  base: '/my-portfolio/',// 🔁 Change this to your repo name
   plugins: [react()],
 })
 
